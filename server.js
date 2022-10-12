3b87b0250cb61c725cf4d9a69f42c78df8bcfe5f
ddenoel <dianedenoel@orange.fr>
2022-10-12 02:06:11 +0200

    forgot this because im an idiot

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/ngx-theme'));
+app.use(express.static(__dirname + '/dist/ngx-theme-demo'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/ngx-theme/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ngx-theme-demo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
