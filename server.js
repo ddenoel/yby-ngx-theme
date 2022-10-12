404841c7cc9fc30d7abce7d4dcad66e61fd60243
ddenoel <dianedenoel@orange.fr>
2022-10-12 02:03:31 +0200

    server for heroku deploy

---
+++ server.js
@@ -0,0 +1,15 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/ngx-theme'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/ngx-theme/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
